ba92f20f12f87a9ea67752906e253032860c8d74
Emil Arran Baleva <arranbaleva@gmail.com>
2018-02-26 23:28:33 +0800

    Added model and template, couldn't figure out how to make a working view

--- marsrover/wallpaper/models.py
+++ marsrover/wallpaper/models.py
@@ -1,6 +1,37 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+# class Rover(models.Model):
+#     rover_id = models.IntegerField(primary_key=True)
+#     rover_name = models.CharField(max_length=50)
+#     landing_date = models.DateField()
+#     launch_date = models.DateField()
+#     status = models.DateField()
+#     max_sol = models.IntegerField()
+#     max_date = models.DateField()
+#     total_photos = models.IntegerField()
+
+
+# class Camera(models.Model):
+#     camera_id = models.IntegerField(primary_key=True)
+#     camera_name = models.CharField(max_length=50)
+#     rover_id = models.ForeignKey()
+#     camera_fullname = models.CharField(max_length=100)
+
+
+class Image(models.Model):
+    sol = models.IntegerField()
+    camera_id = models.IntegerField()
+    earth_date = models.DateField()
+    img_src = models.CharField(max_length=500)
+    date_saved = models.DateField()
+
+    def was_saved_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(0, 43200, 0) <= self.date_saved <= now
--- marsrover/wallpaper/views.py
+++ marsrover/wallpaper/views.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+import wallpaper
 # Create your views here.
+
+
+def homepage(request):
+    data = wallpaper.curiosityrover()['photos'][0]
+    image = data['img_src']
+
+    context = {
+        'image': image,
+    }
+
+    return render(request, "wallpaper/index.html", context)
