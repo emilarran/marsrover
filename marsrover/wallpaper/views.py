76518a7f3c70989d74688d2ac4c47d298e3a0994
Emil Arran Baleva <arranbaleva@gmail.com>
2018-02-28 20:42:16 +0800

    Print rover image in template. Still using function-based views, needs to save in models, and needs refreshing every 12 hours.

--- marsrover/wallpaper/views.py
+++ marsrover/wallpaper/views.py
@@ -2,17 +2,49 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views import generic
+from django.views.generic import TemplateView
+from django.http import HttpResponse
 
-import wallpaper
+import requests
+import json
 # Create your views here.
 
 
 def homepage(request):
-    data = wallpaper.curiosityrover()['photos'][0]
-    image = data['img_src']
-
+    something = curiosityrover().content
+    
+    data = json.loads(something)
+    image = data['photos'][0]
+    image = image['img_src']
+    print(image)
     context = {
         'image': image,
     }
 
     return render(request, "wallpaper/index.html", context)
+
+
+def curiosityrover():
+    url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1"
+    app_token = "ApEztOfnuLutAfibTJmSuorcTQMHxQkozbocepVm"
+    payload = {'api_key': app_token}
+    response = requests.get(url, payload)
+    response = json.dumps(response.json())
+    return HttpResponse(response)
+
+
+
+# class HomeView(TemplateView):
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+
+
+#         def curiosityrover():
+#             url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1"
+#             app_token = "ApEztOfnuLutAfibTJmSuorcTQMHxQkozbocepVm"
+#             payload = {'api_key': app_token}
+#             print(payload)
+#             return requests.get(url, payload,))
+
+#         context['image_url'] = 
