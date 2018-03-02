fe289a54b5f30c63376f11a32ba89a8550a4d178
Emil Arran Baleva <arranbaleva@gmail.com>
2018-03-02 19:53:33 +0800

    Render background-image using fetch.

--- marsrover/wallpaper/urls.py
+++ marsrover/wallpaper/urls.py
@@ -5,5 +5,5 @@ from .views import HomeView, Something
 urlpatterns = [
     url(r'^home/$', HomeView.as_view(), name='homepage'),
     url(r'^get-image/$', Something.as_view(), name='homepage'),
-    url(r'^$', views.homepage, name='homepage'),
+    # url(r'^$', views.homepage, name='homepage'),
 ]
--- marsrover/wallpaper/views.py
+++ marsrover/wallpaper/views.py
@@ -14,35 +14,37 @@ from django.views.generic import TemplateView, View
 # Create your views here.
 
 
-def homepage(request):
-    data = curiosityrover()
-    #data = json.loads(something)
-    image = data['photos'][0]
-    image = image['img_src']
-    context = {
-        'image': image,
-    }
+# def homepage(request):
+#     data = curiosityrover()
+#     #data = json.loads(something)
+#     image = data['photos'][0]
+#     image = image['img_src']
+#     context = {
+#         'image': image,
+#     }
 
-    return render(request, "wallpaper/index.html", context)
+#     return render(request, "wallpaper/index.html", context)
 
 
-def curiosityrover():
-    url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1"
-    app_token = "ApEztOfnuLutAfibTJmSuorcTQMHxQkozbocepVm"
-    payload = {'api_key': app_token}
-    response = requests.get(url, payload)
-    #response = json.dumps(response.json())
-    return response.json()
+# def curiosityrover():
+#     url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1"
+#     app_token = "ApEztOfnuLutAfibTJmSuorcTQMHxQkozbocepVm"
+#     payload = {'api_key': app_token}
+#     response = requests.get(url, payload)
+#     #response = json.dumps(response.json())
+#     return response.json()
 
 
 class HomeView(TemplateView):
     template_name = "wallpaper/index.html"
 
+
 class Something(View):
     def get(self, request, **kwargs):
-        now = timezone.now()
-        datestart = now - datetime.timedelta(0, 43200, 0)
-        param = Image.objects.filter(date_saved__range=(datestart, now))
+        # now = timezone.now()
+        # datestart = now - datetime.timedelta(0, 43200, 0)
+        # param = Image.objects.filter(date_saved__range=(datestart, now))
+
         def curiosityrover():
             url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1"
             app_token = "ApEztOfnuLutAfibTJmSuorcTQMHxQkozbocepVm"
@@ -59,61 +61,11 @@ class Something(View):
                     date_saved=timezone.now(),
                     )
         mod.save()
-        # context['sol'] = image['sol']
-        # context['camera_id'] = image['camera_id']
-        # context['earth_date'] = image['earth_date']
-        # context['img_src'] = image['img_src']
-        # context['date_saved'] = image['date_saved']
         context = {
             'sol': image['sol'],
             'earth_date': image['earth_date'],
             'img_src': image['img_src'],
             'date_saved': timezone.now().isoformat(),
         }
-        return HttpResponse(json.dumps(context),content_type="text/json")
+        return HttpResponse(json.dumps(context), content_type="text/json")
         # return JsonResponse(context)
-
-
-#     def get_context_data(self, **kwargs):
-#         #context = super().get_context_data(**kwargs)
-#         context = {}
-#         def curiosityrover():
-#             url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1"
-#             app_token = "ApEztOfnuLutAfibTJmSuorcTQMHxQkozbocepVm"
-#             payload = {'api_key': app_token}
-#             response = requests.get(url, payload)
-#             data = response.json()
-#             image = data['photos'][0]
-#             return image['img_src']
-
-#         context['image_url'] = curiosityrover()
-
-
-# def get_context_data(self, **kwargs):
-#             #context = super().get_context_data(**kwargs)
-#             context = {}
-
-#             def curiosityrover():
-#                 url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1"
-#                 app_token = "ApEztOfnuLutAfibTJmSuorcTQMHxQkozbocepVm"
-#                 payload = {'api_key': app_token}
-#                 response = requests.get(url, payload)
-#                 data = response.json()
-#                 image = data['photos'][0]
-#                 return image
-
-#             image = curiosityrover()
-#             mod = Image(sol=image['sol'],
-#                         camera_id=image['camera_id'],
-#                         earth_date=image['earth_date'],
-#                         image_src=image['image_src'],
-#                         date_saved=image['date_saved'],
-#                         )
-#             mod.save()
-#             context = {
-#                 'sol': image['sol'],
-#                 'camera_id': image['camera_id'],
-#                 'earth_date': image['earth_date'],
-#                 'image_src': image['image_src'],
-#                 'date_saved': image['date_saved'],
-#             }
